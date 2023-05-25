b3745618822b9a0f8b9499dfebb57de4fbca8594
pajakoo <georgievkn82@gmail.com>
2023-05-25 17:36:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,10 @@
 const express = require('express')
 const app = express()
-const { MongoClient } = require('mongodb');
-
-
-const uri = "mongodb+srv://georgievkn82:S2UNdFGTzPCVz9TE@cluster0.udwqatw.mongodb.net/ShoppingApp?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
+ 
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
-
-app.get('/api/products', async (req, res) => {
-  try {
-    const db = client.db('ShoppingApp');
-    const products = await db.collection('products').find().toArray();
-    res.json(products);
-  } catch (err) {
-    console.error('Грешка при търсене на продуктите', err);
-    res.status(500).json({ error: 'Възникна грешка' });
-  }
-});
-
+ 
 
 app.listen(process.env.PORT || 3000)
